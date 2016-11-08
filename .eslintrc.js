ce0764df8647e9b9dc3835e26db0f735a248d0c5
Tomi Turtiainen <tomi.turtiainen@gmail.com>
2016-11-08 22:18:20 +0200

    Add eslint

---
+++ .eslintrc.js
@@ -0,0 +1,15 @@
+module.exports = {
+	"parser": "babel-eslint",
+	"env": {
+		"browser": true
+	},
+	"plugins": [
+		"react"
+	],
+	"extends": [
+		"eslint:recommended",
+		"plugin:react/recommended"
+	],
+	"rules": {
+	}
+};
